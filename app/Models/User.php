52b9d0669e54b8954e26bbe0938efd2a13b46c08
Kaka <kakalition@gmail.com>
2022-05-03 18:58:23 +0700

    Create Comment model and its migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use \app\Models\SavedMeditation;
-use \app\Models\SavedBreathing;
-use \app\Models\History;
+use App\Models\SavedMeditation;
+use App\Models\SavedBreathing;
+use App\Models\History;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
---
+++ database/migrations/2022_05_03_115325_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Meditation;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Meditation::class);
+            $table->bigInteger('user_id');
+            $table->date('date');
+            $table->string('comment');
+            $table->decimal('rating', 2, 1, true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
