9724cb784a5970edcdb1e057d0221fa76921efa0
Kaka <kakalition@gmail.com>
2022-05-05 11:45:49 +0700

    Create first version of SingleForm component

--- public/js/app.js
+++ public/js/app.js
@@ -2179,6 +2179,7 @@ __webpack_require__(/*! ./auth/register/RegisterPage */ "./resources/js/auth/reg
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "SingleForm": () => (/* binding */ SingleForm),
 /* harmony export */   "default": () => (/* binding */ RegisterPage)
 /* harmony export */ });
 /* harmony import */ var _utils_ElementBinder__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../utils/ElementBinder */ "./resources/js/utils/ElementBinder.js");
@@ -2186,8 +2187,29 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+
+function SingleForm(type, id, label, placeholder, inlineAction) {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment, {
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+      className: "flex w-full items-center justify-between",
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("label", {
+        htmlFor: id,
+        className: "font-ibm-plex-sans text-xl text-black",
+        children: label
+      }), inlineAction]
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
+      className: "h-2"
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("input", {
+      className: "h-14 w-full border-2 border-x-transparent border-t-transparent border-b-slate-400 bg-slate-100 px-4 font-ibm-plex-sans   focus:border-2 focus:border-primary-button focus:outline-none",
+      type: type,
+      id: id,
+      name: id,
+      placeholder: placeholder,
+      required: true
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("br", {})]
+  });
+}
 function RegisterPage(props) {
-  var bgClass = "bg-[".concat(props.publicpath + "/bg.png", "]");
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
     className: "flex min-h-screen w-screen items-center justify-center bg-cover bg-center",
     style: {
@@ -2207,11 +2229,19 @@ function RegisterPage(props) {
           children: "Create new account"
         })]
       }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
-        className: "h-12"
+        className: "h-8"
       }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
-        className: "border-t-2 border-t-slate-300 mx-8"
+        className: "mx-8 border-t-2 border-t-slate-300"
       }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
-        className: "h-12"
+        className: "h-8"
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
+        className: "my-2 w-full px-8",
+        children: SingleForm("text", "test", "Email address", "yourname@provider.com")
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
+        className: "h-4"
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
+        className: "my-2 w-full px-8",
+        children: SingleForm("password", "test1", "Password", "••••••••")
       })]
     })
   });
--- resources/js/auth/register/RegisterPage.js
+++ resources/js/auth/register/RegisterPage.js
@@ -1,8 +1,36 @@
 import { elementBinder } from "../../utils/ElementBinder";
 
-export default function RegisterPage(props) {
-    const bgClass = `bg-[${props.publicpath + "/bg.png"}]`;
+export function SingleForm(type, id, label, placeholder, inlineAction) {
+    return (
+        <>
+            <div className="flex w-full items-center justify-between">
+                <label
+                    htmlFor={id}
+                    className="font-ibm-plex-sans text-xl text-black"
+                >
+                    {label}
+                </label>
 
+                {inlineAction}
+            </div>
+
+            <div className="h-2" />
+
+            <input
+                className="h-14 w-full border-2 border-x-transparent border-t-transparent border-b-slate-400 bg-slate-100 px-4 font-ibm-plex-sans  
+                focus:border-2 focus:border-primary-button focus:outline-none"
+                type={type}
+                id={id}
+                name={id}
+                placeholder={placeholder}
+                required
+            />
+            <br />
+        </>
+    );
+}
+
+export default function RegisterPage(props) {
     return (
         <div
             className={`flex min-h-screen w-screen items-center justify-center bg-cover bg-center`}
@@ -22,9 +50,24 @@ export default function RegisterPage(props) {
                     </span>
                 </h2>
 
-                <div className="h-12" />
-                <div className="border-t-2 border-t-slate-300 mx-8" />
-                <div className="h-12" />
+                <div className="h-8" />
+                <div className="mx-8 border-t-2 border-t-slate-300" />
+                <div className="h-8" />
+
+                <div className="my-2 w-full px-8">
+                    {SingleForm(
+                        "text",
+                        "test",
+                        "Email address",
+                        "yourname@provider.com"
+                    )}
+                </div>
+
+                <div className="h-4" />
+
+                <div className="my-2 w-full px-8">
+                    {SingleForm("password", "test1", "Password", "••••••••")}
+                </div>
             </div>
         </div>
     );
