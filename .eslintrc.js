1e0d3a55f737db87c367b36cd52647c0b03e46be
Kaka <kakalition@gmail.com>
2022-05-06 07:52:29 +0700

    Add ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:react/jsx-runtime',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    extends: ["plugin:react/recommended", "airbnb", "plugin:react/jsx-runtime"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {},
 };
