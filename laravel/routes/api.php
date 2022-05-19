c4f4f22043ac5a01bd2ccbe349ec68dce1784e1f
Kaka <kakalition@gmail.com>
2022-05-20 06:52:28 +0700

    Remove not working part of code

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -16,9 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('getUser', function(Request $request) {
-  return json_encode(Auth::user());
+  return $request->user();
 });
--- react/src/features/auth/login/utils/LoginCallbackBuilder.js
+++ react/src/features/auth/login/utils/LoginCallbackBuilder.js
@@ -4,13 +4,9 @@ import axios from 'axios';
 export default function loginCallbackBuilder(shouldAnimateSetter, errorMessageSetter, errorListSetter) {
   return (e) => {
     e.preventDefault();
+    console.log(document.querySelector('meta[name=\'csrf-token\''));
 
     const formData = new FormData(document.getElementById('login-form'));
-    const config = {
-      headers: {
-        'X-CSRF-TOKEN': document.querySelector('meta[name=\'csrf-token\'').content,
-      },
-    };
 
     const onFulfilled = (response) => {
       if (response.status === 200) {
@@ -26,8 +22,11 @@ export default function loginCallbackBuilder(shouldAnimateSetter, errorMessageSe
       setTimeout(() => shouldAnimateSetter(false), 5400);
     };
 
-    axios
-      .post('/login', formData, config)
-      .then(onFulfilled, onRejected);
+    axios({
+      method: 'post',
+      url: 'http://localhost/login',
+      data: formData,
+      headers: { 'X-CSRF-TOKEN': document.querySelector('meta[name=\'csrf-token\'') },
+    }).then(onFulfilled, onRejected);
   };
 }
