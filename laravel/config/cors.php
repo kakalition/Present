854380efeea9d03b2db63bd4658e9357a8b7ec17
Kaka <kakalition@gmail.com>
2022-05-20 10:27:47 +0700

    Modify useAuth to use axios; create getUser api route

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-  'paths' => ['api/*', 'sanctum/csrf-cookie', 'login'],
+  'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'home'],
 
   'allowed_methods' => ['*'],
 
