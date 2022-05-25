002b152f813a25ba9edd324d037b81afd730fae9
Kaka <kakalition@gmail.com>
2022-05-25 09:03:17 +0700

    Modify add button in MarketplaceDetailCommentComponent to have hover and active color state

--- react/src/features/marketplace/detail/components/MarketplaceDetailCommentComponent.tsx
+++ react/src/features/marketplace/detail/components/MarketplaceDetailCommentComponent.tsx
@@ -17,7 +17,12 @@ export default function MarketplaceDetailCommentComponent({
     <div className="flex flex-col w-[60%] pl-16 pb-16">
       <div className="flex flex-row w-full items-center justify-between">
         <h2 className="font-ibm-plex-sans text-black text-5xl font-medium">Comments</h2>
-        <div className="h-8 w-8 stroke-2 stroke-black"><PlusIcon /></div>
+        <button
+          className="h-16 w-16 p-4 rounded-full bg-web-bg hover:bg-[#dbdbdb] active:bg-[#b1b1b1] flex items-center justify-center transition duration-75"
+          type="button"
+        >
+          <div className="h-full w-full stroke-2 stroke-black"><PlusIcon /></div>
+        </button>
       </div>
       <div className="h-4" />
       {elements}
