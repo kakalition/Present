2faf98635d99bc11b5e108106ac7bf5f624228ed
Kaka <kakalition@gmail.com>
2022-05-27 20:10:24 +0700

    Implement correct behavior of audio progress bar

--- react/src/features/session/meditation/components/AudioTrackerComponent.tsx
+++ react/src/features/session/meditation/components/AudioTrackerComponent.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
+
 export default function AudioTracker({
   currentDuration, duration,
 }: { currentDuration: number, duration: number }) {
   const roundedCurrentDuration = Math.round(currentDuration);
+  const w = `${(currentDuration / duration) * 100}%`;
+  useEffect(() => {
+    console.log(w);
+  }, [w]);
 
   return (
     <div className="flex flex-col w-full">
       <div className="relative w-full h-2">
         <div className="absolute w-full h-full bg-slate-600 rounded-full" />
-        <div className="absolute z-[1] w-1/2 h-full bg-white rounded-full" />
+        <div
+          className="absolute z-[1] h-full bg-white rounded-full"
+          style={{ width: w }}
+        />
       </div>
       <div className="h-4" />
       <div className="flex flex-row justify-between items-center w-full h-2">
